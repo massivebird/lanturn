22905cc264721f63af0cfe35b9fa44d250d0d5e3
massivebird <33991373+massivebird@users.noreply.github.com>
2024-11-28 14:59:30 -0500

    added(flake): packages

--- flake.nix
+++ flake.nix
@@ -3,29 +3,35 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    naersk = {
+      url = "github:nix-community/naersk/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, naersk, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
       in
         with pkgs;
       {
+        packages.default = naersk-lib.buildPackage ./.;
 
-        # `nix develop`
-        devShell = mkShell {
-          packages = [
-            cargo
-            openssl
-            pkg-config
-            clippy
-            rust-analyzer
-            rustc
-          ];
-        };
+        # for `nix develop`:
+        shells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              cargo
+              openssl
+              pkg-config
+              clippy
+              rust-analyzer
+              rustc
+            ];
+          };
       }
     );
 }
